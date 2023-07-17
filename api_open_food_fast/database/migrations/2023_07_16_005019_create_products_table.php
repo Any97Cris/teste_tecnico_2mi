cddf401f059616cbc7410387f6c0bb8cd1bbb9b9
Any97Cris <criscianysilva1997@gmail.com>
2023-07-16 20:38:37 -0400

    criacao da migration tabela products e campos

--- api_open_food_fast/database/migrations/2023_07_16_005019_create_products_table.php
+++ api_open_food_fast/database/migrations/2023_07_16_005019_create_products_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Type\Integer;
+use Illuminate\Support\Facades\DB;
+
 
 return new class extends Migration
 {
@@ -14,29 +17,29 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->integer('code')->primary()->nullable();
-            $table->string('status');
-            $table->dateTime('imported_t');
-            $table->string('url');
-            $table->string('creator');
-            $table->string('created_t');
-            $table->string('last_modified_t');
-            $table->string('product_name');
-            $table->string('quantity');
-            $table->string('brands');
-            $table->string('categories');
-            $table->string('labels');
-            $table->string('cities');
-            $table->string('purchase_places');
-            $table->string('stores');
-            $table->text('ingredients_text');
-            $table->string('traces');
-            $table->string('serving_size');
-            $table->float('serving_quantity');
-            $table->float('nutriscore_score');
-            $table->char('nutriscore_grade');
-            $table->string('main_category');
-            $table->string('image_url');
+            $table->string('code')->primary();
+            $table->enum('status',['draft', 'trash', 'published'])->default('published');
+            $table->timestamp('imported_t')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('url')->nullable();
+            $table->string('creator')->nullable();
+            $table->string('created_t')->nullable();
+            $table->string('last_modified_t')->nullable();
+            $table->string('product_name')->nullable();
+            $table->string('quantity')->nullable();
+            $table->string('brands')->nullable();
+            $table->text('categories')->nullable();
+            $table->text('labels')->nullable();
+            $table->string('cities')->nullable();
+            $table->string('purchase_places')->nullable();
+            $table->string('stores')->nullable();
+            $table->text('ingredients_text')->nullable();
+            $table->string('traces')->nullable();
+            $table->string('serving_size')->nullable();
+            $table->float('serving_quantity')->nullable();
+            $table->float('nutriscore_score')->nullable();
+            $table->char('nutriscore_grade')->nullable();
+            $table->string('main_category')->nullable();
+            $table->string('image_url')->nullable();
             $table->timestamps();
         });
     }
