34d39e6f6f5e2f451313323a8b9947970656f583
Any97Cris <criscianysilva1997@gmail.com>
2023-07-16 20:39:20 -0400

    ajuste do nome da chave primary

--- api_open_food_fast/app/Models/Product.php
+++ api_open_food_fast/app/Models/Product.php
@@ -9,7 +9,9 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['code','imported_t','url','creator','created_t','last_modified_t',
+    protected $primaryKey = 'code';
+
+    protected $fillable = ['code','status','imported_t','url','creator','created_t','last_modified_t',
                            'product_name','quantity','brands','categories','labels','cities',
                            'purchase_places','stores','ingredients_text','traces','serving_size',
                            'serving_quantity','nutriscore_score','nutriscore_grade','main_category',
